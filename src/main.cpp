a5d0bfecfc0526a841727b1f32f7270d5e7f7bcc
PortalPC <ian.portal@usil.pe>
2025-05-05 12:36:38 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+char a = 0;
+void config_IE (void){
+  EIMSK |= (1<<int0);
+  EICRA |= (1<<ISC01);
+  DDRD &= ~(0x04);
+  PORTD |= 0X04;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ISR (INT0_vect){
+  a++;
+  if(a>1){
+    a = 0;
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main(void){
+  DDRD |= 0xE0;
+  PORTD &= ~(0xE0);
+  DDRB |= 0x07;
+  config_IE();
+  sei();
+
+  while(1){
+    if(a==1){
+      PORTD &= ~(0XE0);
+      PORTB &= ~(0x07);
+
+      PORTD |= 0x20;
+      PORTB |= 0X04;
+      _delay_ms(5000);
+      PORTD &= ~(0XE0);
+      PORTB &= ~(0x07);
+      PORTD |= 0x40;
+      PORTB |= 0X04;
+      _delay_ms(1000);
+      PORTD &= ~(0XE0);
+      PORTB &= ~(0x07);
+      PORTD |= 0x80;
+      PORTB |= 0X01;
+
+      _delay_ms(5000);
+      
+    }
+  }
 }
